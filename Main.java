642191a35881e7d7d3e2360f94f88251f617a584
mohamed <abdobenarbia8@gmail.com>
2026-03-24 11:09:15 +0100

    update

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
   public static void main(String[] args){
-    System.out.println("Hello new version........");
+    System.out.println("EVHHBHBGCFFCGVBHBBJHJVHJVVJVJVJVJGJVGVVJHVJHJVVVVVVVVVVVVVVVHHnew version........");
   }
 }
 
